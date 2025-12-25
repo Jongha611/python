7e8381f0bdef00bbf2bdbe5fcfea90dea4361dd5
Jongha611 <kimjongha611@gmail.com>
2025-12-25 23:10:57 +0900

    4섹션 초반

---
+++ section_4.py
@@ -0,0 +1,24 @@
+# Assignment Operator
+# Assignment with an = on lists does not make a copy. 
+# Instead, assignment makes the two variables point to the one list in memory.
+
+# colors는 리스트 객체를 포인트 했음. b = colors 라는 별개의 카피 객체를 가지고 있는 것이 아니다.
+# b와 colors 라는 변수 둘 다 원본 list object의 address를 포인팅 하고 있다.
+colors = ["red", "blue", "yellow"]
+b = colors
+
+b.append("white")
+print(b, colors)
+
+a = [[1, 2], [2, 4]]
+b = a[:] # shallow copy
+
+b[0].append([3, 6])
+
+print(a)
+print(b)
+
+b[0].append(7)
+
+print(a)
+print(b)
