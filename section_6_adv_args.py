c267a0309d9a8c83fc1a6be2a93f243082a4dfb0
Jongha611 <kimjongha611@gmail.com>
2026-01-01 20:05:24 +0900

    checkpoint3

---
+++ section_6_adv_args.py
@@ -0,0 +1,40 @@
+# Arguments with Default Values
+def write_str_to_file(file_name, content, mode="w"):
+    with open(file_name, encoding="utf-8", mode=mode) as f:
+        f.write(content)
+        
+write_str_to_file(
+    file_name="default_values.md",
+    content="\nhello world",
+    mode="a" # append를 의미    
+)
+
+
+# Unlimited Positional Arguments
+def unlimited_func(*args):
+    print(args)
+unlimited_func(1, 2, 3)
+
+def sum_all(*args):
+    total = 0
+    for n in args:
+        total += n
+    return total
+print(sum_all(1, 2, 3, 4, 5, 6))
+
+
+# **kwargs: Keyworded arguments
+def calc(**kwargs):
+    print(kwargs)
+    
+calc(n1=1, n2=3, func="add")
+
+def calc(**kwargs):
+    for k, v in kwargs.items():
+        print(k)
+        print(v)
+        
+    if kwargs["func"] == "add":
+        return kwargs["n1"] + kwargs["n2"]
+    
+print(calc(n1=1, n2=3, func="add"))
