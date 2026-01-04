8970b5b5988f936fba6cbb2c9f8366a5f3a78b61
Jongha611 <kimjongha611@gmail.com>
2026-01-04 17:08:39 +0900

    section_6_end

--- section_6_adv_args.py
+++ section_6_adv_args.py
@@ -6,21 +6,21 @@ def write_str_to_file(file_name, content, mode="w"):
 write_str_to_file(
     file_name="default_values.md",
     content="\nhello world",
-    mode="a" # append를 의미    
+    mode="a" # append를 의미  # mode = mode = "W"로 함수설계에서 정의했으므로  
 )
 
 
-# Unlimited Positional Arguments
-def unlimited_func(*args):
-    print(args)
-unlimited_func(1, 2, 3)
+# # Unlimited Positional Arguments
+# def unlimited_func(*args):
+#     print(args)
+# unlimited_func(1, 2, 3)
 
-def sum_all(*args):
-    total = 0
-    for n in args:
-        total += n
-    return total
-print(sum_all(1, 2, 3, 4, 5, 6))
+# def sum_all(*args): # *args
+#     total = 0
+#     for n in args:
+#         total += n
+#     return total
+# print(sum_all(1, 2, 3, 4, 5, 6))
 
 
 # **kwargs: Keyworded arguments
