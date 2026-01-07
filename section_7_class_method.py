0f17fedd1599fee3af7bd70f85e6c311fd76143b
Jongha611 <kimjongha611@gmail.com>
2026-01-07 10:49:36 +0900

    checkpoint3

--- section_7_class_method.py
+++ section_7_class_method.py
@@ -1,6 +1,6 @@
 class Car():
     def __init__(self, body_type):
-        self.bodytype = body_type
+        self.body_type = body_type
         
     def __repr__(self):
         return f"Car({self.body_type} body type)"
